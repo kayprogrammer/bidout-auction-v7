dfda2cbb298ea99661ecfb453ac729a648759a84
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2023-12-25 19:13:34 +0100

    chore: added user id to to short user data

--- models/base.go
+++ models/base.go
@@ -18,6 +18,7 @@ type File struct {
 }
 
 type ShortUserData struct {
+	ID					uuid.UUID			`json:"id" example:"2c64c881-59ca-4916-b2bc-8cfb75c3f09b"`
 	Name				string				`json:"name" example:"John Doe"`
 	Avatar				*string				`json:"avatar" example:"https://my-avatar.com"`
 }
